c9a99e279d184f7f3232227a6d5f32d0475ba340
patricia145 <patriciavaldez0517@gmail.com>
2024-09-15 19:54:42 +0800

    initial commit

--- strings.js
+++ strings.js
@@ -1,16 +1,16 @@
 //Create a variable named favActorFirstName and store your favorite actor's first name
-let favActorFirstName = "Hugh";
+let favActorFirstName = "Arcilla";
 //Create a variable named favActorLastName and store their last name
-let favActorLastName = "Jackman";
+let favActorLastName = "John";
 let fullName = favActorFirstName + "" + favActorLastName;
 
 let uppercase = fullName.toLowerCase();
 
-let lowercase = "Torgal".toUpperCase();
+let lowercase = "Kikay".toUpperCase();
 
 let message = "My favorite actor is " + fullName.toUpperCase();
 
-message += ",his best show is Silicon Valley.";
+message += ",his best show is Heneral Luna.";
 
 console.log(favActorFirstName);
 console.log(favActorLastName);
